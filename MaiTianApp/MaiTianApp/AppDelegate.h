5638d5e8490c35f096b71f5ec3560192bd2e7d18
Xuhai <1135293165@qq.com>
2016-12-08 20:51:35 +0800

    上传初始化工程

---
+++ MaiTianApp/MaiTianApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MaiTianApp
+//
+//  Created by tarena001 on 16/12/8.
+//  Copyright © 2016年 MaiTianApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
